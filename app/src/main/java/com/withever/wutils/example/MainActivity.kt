650cb6368002e0f11fdfec76d98d76581d57e1de
rlawldhsk <rlawldhsk@gmail.com>
2019-03-11 22:13:52 +0900

    recreate proejct
    recreate proejct

--- app/src/main/java/com/withever/wutils/example/MainActivity.kt
+++ app/src/main/java/com/withever/wutils/example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.withever.wutils
+package com.withever.wutils.example
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/withever/wutils/extension/WViewExtension.kt
+++
@@ -1,7 +0,0 @@
-package com.withever.wutils.extension
-
-import android.app.Activity
-import android.support.annotation.LayoutRes
-import android.view.View
-
-fun Activity.inflate(@LayoutRes layout: Int): View = this.layoutInflater.inflate(layout, null)
